d9138c24e63f1815d2634126d1709de3d825904e
Jonathan Woodard <jvw.226@gmail.com>
2023-02-09 14:56:33 -0600

    added scheduling ability to script to send email every morning

--- gamescraper.py
+++ gamescraper.py
@@ -7,57 +7,73 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import ssl
 from dotenv import load_dotenv
+import datetime
+import pytz
+import schedule
+import time as tm
+
+
+# dt = datetime.datetime.now(tz=pytz.timezone("US/Central"))
+
 
 load_dotenv()
 
-source = requests.get("https://sportsgamestoday.com/").text
 
-soup = BeautifulSoup(source, "lxml")
-# print(soup.prettify())
+def send_email():
 
-title = soup.find("div", class_="gametitle").text
+    source = requests.get("https://sportsgamestoday.com/").text
 
-games_table = soup.find("table")
-# print(games_table)
+    soup = BeautifulSoup(source, "lxml")
+    # print(soup.prettify())
 
-# for game in games_table.find_all('tr'):
+    # title = soup.find("div", class_="gametitle").text
 
-#     full_game_schedule = game.text
+    games_table = soup.find("table")
+    # print(games_table)
+
+    # for game in games_table.find_all('tr'):
+
+    #     full_game_schedule = game.text
+
+    # if game.find('th'):
+    #     section_title = soup.find('th', class_='gamematchuptitle')
+    #     game_time_title = soup.find('th', class_='gametimetitle')
+    #     section_channel = soup.find('th', class_='gamechanneltitle')
+    #     print(section_title)
+
+    #     print(game_time_title)
+    #     print(section_channel)
+    # else:
+    #     game_title = soup.find('td', class_='gamematchup')
+    #     game_time = soup.find('td', class_='gametime')
+    #     channel_name = soup.find('td', class_='gamechannel')
+    #     print(game_title)
+    #     print(game_time)
+    #     print(channel_name)
+
+    sender_email = os.getenv("SENDER_EMAIL")
+    receiver_email = os.getenv("RECEIVER_EMAIL")
+    sender_password = os.getenv("SENDER_PASS")
+    # message = games_table
+    html = games_table
+
+    subject = "Check out today's games!"
+
+    email_message = MIMEMultipart()
+    email_message["From"] = sender_email
+    email_message["To"] = receiver_email
+    email_message["Subject"] = subject
+    email_message.attach(MIMEText(html, "html"))
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(sender_email, sender_password)
+        smtp.sendmail(sender_email, receiver_email, email_message.as_string())
 
 
-# if game.find('th'):
-#     section_title = soup.find('th', class_='gamematchuptitle')
-#     game_time_title = soup.find('th', class_='gametimetitle')
-#     section_channel = soup.find('th', class_='gamechanneltitle')
-#     print(section_title)
-#     print(game_time_title)
-#     print(section_channel)
-# else:
-#     game_title = soup.find('td', class_='gamematchup')
-#     game_time = soup.find('td', class_='gametime')
-#     channel_name = soup.find('td', class_='gamechannel')
-#     print(game_title)
-#     print(game_time)
-#     print(channel_name)
+schedule.every().day.at("9:00").do(send_email)
 
-sender_email = os.getenv("SENDER_EMAIL")
-receiver_email = os.getenv("RECEIVER_EMAIL")
-sender_password = os.getenv("SENDER_PASS")
-# message = games_table
-html = games_table
-
-subject = "Check out today's games!"
-body = f"{games_table}"
-
-email_message = MIMEMultipart()
-email_message["From"] = sender_email
-email_message["To"] = receiver_email
-email_message["Subject"] = subject
-email_message.attach(MIMEText(html, "html"))
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-    smtp.login(sender_email, sender_password)
-    smtp.sendmail(sender_email, receiver_email, email_message.as_string())
-server = smtplib.SMTP("smtp.gmail.com", 587)
+while True:
+    schedule.run_pending()
+    tm.sleep(1)
