3b8e951c96cdf16a737e02aa11f12323cdf40598
Jonathan Woodard <jvw.226@gmail.com>
2023-02-18 09:10:34 -0600

    refactored scheduler

--- gamescraper.py
+++ gamescraper.py
@@ -72,7 +72,7 @@ def send_email():
         smtp.sendmail(sender_email, receiver_email, email_message.as_string())
 
 
-schedule.every().day.at("9:15").do(send_email)
+schedule.every().day.at("09:15").do(send_email)
 
 while True:
     schedule.run_pending()
